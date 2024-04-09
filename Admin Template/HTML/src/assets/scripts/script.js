47f741beb944677d732b7077bd3483feacab58b0
M. Fadil Martias <64735243+fadilmartias@users.noreply.github.com>
2024-04-09 23:16:57 +0700

    update folder structures and names

---
+++ Admin Template/HTML/src/assets/scripts/script.js
@@ -0,0 +1,57 @@
+document.querySelector('.btn-close-topbar').addEventListener('click', function () {
+    if (document.querySelector('.sidebar').classList.contains('active')) {
+        document.querySelector('.topbar').classList.remove('active')
+        document.querySelector('.sidebar').classList.remove('active')
+        document.querySelector('.content-wrapper').classList.replace('pl-60', 'pl-0')
+        document.querySelector('.sidebar .wrapper').classList.replace('flex', 'hidden')
+    } else {
+        document.querySelector('.topbar').classList.add('active')
+        document.querySelector('.sidebar').classList.add('active')
+        document.querySelector('.content-wrapper').classList.replace('pl-0', 'pl-60')
+        document.querySelector('.sidebar .wrapper').classList.replace('hidden', 'flex')
+    }
+});
+document.addEventListener('click', function (event) {
+    const dropdown = document.querySelector('.dropdown');
+    const userInfo = document.querySelector('.user-info');
+
+    // Jika yang diklik bukan bagian dari dropdown atau user-info
+    if (!dropdown.contains(event.target) && !userInfo.contains(event.target)) {
+        dropdown.classList.remove('active');
+    }
+});
+
+document.querySelector('.user-info').addEventListener('click', function () {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('active');
+});
+
+document.querySelectorAll('.dropdown-sidebar').forEach(function (element) {
+    element.addEventListener('click', function () {
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.classList.toggle('active');
+    });
+});
+
+// Daftar breadcrumb yang akan ditampilkan
+const breadcrumbItems = ['Components', 'Blank Page']; // Anda bisa mengubah array ini sesuai dengan kebutuhan
+
+// Ambil elemen breadcrumb
+const breadcrumbContainer = document.getElementById('breadcrumb');
+
+// Buat dan tambahkan elemen breadcrumb secara dinamis
+breadcrumbItems.forEach((item, index) => {
+    const breadcrumbItem = document.createElement('div');
+
+    // Tambahkan teks dan class
+    breadcrumbItem.textContent = item;
+
+    // Jika bukan item terakhir, tambahkan tanda "/"
+    if (index < breadcrumbItems.length && index != 0) {
+        const separator = document.createElement('div');
+        separator.textContent = '/';
+        breadcrumbContainer.appendChild(separator);
+    }
+
+    breadcrumbContainer.appendChild(breadcrumbItem);
+});
