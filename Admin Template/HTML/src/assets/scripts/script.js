abc9e2c6b607caca46743509a317936ee82a6dd4
M. Fadil Martias <64735243+fadilmartias@users.noreply.github.com>
2024-04-29 06:41:05 +0700

    add minimize sidebar option

--- Admin Template/HTML/src/assets/scripts/script.js
+++ Admin Template/HTML/src/assets/scripts/script.js
@@ -1,80 +1,54 @@
-document
-  .querySelector(".btn-close-topbar")
-  .addEventListener("click", function () {
-    if (document.querySelector(".sidebar").classList.contains("active")) {
-      document.querySelector(".topbar").classList.remove("active");
-      document.querySelector(".sidebar").classList.remove("active");
-      document
-        .querySelector(".content-wrapper")
-        .classList.replace("pl-60", "pl-0");
-      document.querySelector(".sidebar-icon").classList.replace(`icon-[solar--hamburger-menu-linear]`, `icon-[formkit--arrowright]`)
-      document
-        .querySelector(".sidebar .wrapper")
-        .classList.replace("flex", "hidden");
+$(document).ready(function () {
+  // Daftar breadcrumb yang akan ditampilkan
+  const breadcrumbItems = ["Components", "Blank Page"]; // Anda bisa mengubah array ini sesuai dengan kebutuhan
+
+  // Ambil elemen breadcrumb
+  const breadcrumbContainer = $("#breadcrumb");
+
+  // Buat dan tambahkan elemen breadcrumb secara dinamis
+  breadcrumbItems.forEach((item, index) => {
+    const breadcrumbItem = $("<div></div>");
+
+    // Tambahkan teks dan class
+    breadcrumbItem.text(item);
+
+    // Jika bukan item terakhir, tambahkan tanda "/"
+    if (index < breadcrumbItems.length && index != 0) {
+      const separator = $("<div>/</div>");
+      breadcrumbContainer.append(separator);
+    }
+
+    breadcrumbContainer.append(breadcrumbItem);
+  });
+
+  $(".btn-close-topbar").click(function () {
+    if ($(".sidebar").hasClass("active")) {
+      $(".topbar, .sidebar").removeClass("active");
+      $(".sidebar-icon")
+        .removeClass("icon-[solar--hamburger-menu-linear]")
+        .addClass("icon-[formkit--arrowright]");
+      if ($(".sidebar").hasClass("can-minimize")) {
+        $(".topbar").addClass("pl-28");
+      }
+        // Ubah atribut data-toggle menjadi dropdown jika tidak aktif
+        $('.item-collapse .collapse-sidebar').attr('data-toggle', 'dropdown');
+      // $(".sidebar .wrapper").removeClass("flex").addClass("hidden");
     } else {
-      document.querySelector(".topbar").classList.add("active");
-      document.querySelector(".sidebar").classList.add("active");
-      document
-        .querySelector(".content-wrapper")
-        .classList.replace("pl-0", "pl-60");
-        document.querySelector(".sidebar-icon").classList.replace(`icon-[formkit--arrowright]`, `icon-[solar--hamburger-menu-linear]`)
-      document
-        .querySelector(".sidebar .wrapper")
-        .classList.replace("hidden", "flex");
+      $(".topbar, .sidebar").addClass("active");
+      $(".sidebar-icon")
+        .removeClass("icon-[formkit--arrowright]")
+        .addClass("icon-[solar--hamburger-menu-linear]");
+        if ($(".sidebar").hasClass("can-minimize")) {
+          $(".topbar").removeClass("pl-28");
+        }
+        $('.item-collapse .collapse-sidebar').attr('data-toggle', 'collapse');
+      // $(".sidebar .wrapper").removeClass("hidden").addClass("flex");
     }
   });
 
-$('[data-toggle="dropdown"]').click(function (event) {
-  event.stopPropagation(); // Menghentikan propagasi event agar tidak memicu event document click
-  var dropdown = $(this).next(".dropdown");
-  dropdown.toggleClass("active");
-});
-
-$(document).click(function (event) {
-  var dropdown = $(".dropdown.active");
-  if (
-    !dropdown.is(event.target) &&
-    dropdown.has(event.target).length === 0 &&
-    $('[data-toggle="dropdown"]').has(event.target).length === 0
-  ) {
-    dropdown.removeClass("active");
-  }
-});
-
-// document.querySelectorAll(".dropdown-sidebar").forEach(function (element) {
-//   element.addEventListener("click", function () {
-//     const dropdownContent = this.nextElementSibling;
-//     dropdownContent.classList.toggle("active");
-//   });
-// });
-
-// Daftar breadcrumb yang akan ditampilkan
-const breadcrumbItems = ["Components", "Blank Page"]; // Anda bisa mengubah array ini sesuai dengan kebutuhan
-
-// Ambil elemen breadcrumb
-const breadcrumbContainer = document.getElementById("breadcrumb");
-
-// Buat dan tambahkan elemen breadcrumb secara dinamis
-breadcrumbItems.forEach((item, index) => {
-  const breadcrumbItem = document.createElement("div");
-
-  // Tambahkan teks dan class
-  breadcrumbItem.textContent = item;
-
-  // Jika bukan item terakhir, tambahkan tanda "/"
-  if (index < breadcrumbItems.length && index != 0) {
-    const separator = document.createElement("div");
-    separator.textContent = "/";
-    breadcrumbContainer.appendChild(separator);
-  }
-
-  breadcrumbContainer.appendChild(breadcrumbItem);
-});
-
-$(document).ready(function () {
   // Menambahkan event listener untuk semua elemen dengan data-toggle="modal"
   $('[data-toggle="modal"]').click(function () {
-    var targetModal = $(this).attr("data-target"); // Mendapatkan nilai dari atribut data-target yang diklik
+    const targetModal = $(this).attr("data-target"); // Mendapatkan nilai dari atribut data-target yang diklik
     $(targetModal).addClass("active"); // Menampilkan modal
     $("body").addClass("modal-open"); // Menambahkan class untuk mencegah scrolling background
   });
@@ -105,7 +79,32 @@ $(document).ready(function () {
 
   // Menambahkan event listener untuk semua elemen dengan data-toggle="modal"
   $('[data-toggle="collapse"]').click(function () {
-    var targetCollapse = $(this).attr("data-target"); // Mendapatkan nilai dari atribut data-target yang diklik
+    const targetCollapse = $(this).attr("data-target"); // Mendapatkan nilai dari atribut data-target yang diklik
     $(targetCollapse).toggleClass("active"); // Menampilkan collapse
   });
+
+  // $(".sidebar .item").on("click", function () {
+  //   const link = $(this).find("a").attr("href");
+  //   window.location.href = link;
+  // });
+
+  $('[data-toggle="dropdown"]').click(function (event) {
+    event.stopPropagation(); // Menghentikan propagasi event agar tidak memicu event document click
+    // const dropdown = $(this).next(".dropdown");
+    // dropdown.toggleClass("active");
+    const dropdown = $(this).data("target");
+    $(dropdown).toggleClass("active");
+  });
+
+  $(document).click(function (event) {
+    const dropdown = $(".dropdown.active");
+    if (
+      !dropdown.is(event.target) &&
+      dropdown.has(event.target).length === 0 &&
+      $('[data-toggle="dropdown"]').has(event.target).length === 0
+    ) {
+      dropdown.removeClass("active");
+    }
+  });
+
 });
