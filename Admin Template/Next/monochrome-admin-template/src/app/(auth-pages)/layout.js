86be590474b43a5168e583693336a48e5813de20
M. Fadil Martias <64735243+fadilmartias@users.noreply.github.com>
2024-07-14 18:31:09 +0700

    fix width layout

--- Admin Template/Next/monochrome-admin-template/src/app/(auth-pages)/layout.js
+++ Admin Template/Next/monochrome-admin-template/src/app/(auth-pages)/layout.js
@@ -4,7 +4,7 @@ const AuthLayout = ({ children }) => {
   return (
     <main className="container mx-auto">
       <div className="wrapper flex justify-center items-center min-h-dvh">
-        <div className="sm:border dark:border-gray-600 p-12 rounded-2xl flex flex-col justify-center items-center w-[40%]">
+        <div className="sm:border dark:border-gray-600 p-12 rounded-2xl flex flex-col justify-center items-center w-full sm:w-[60%] lg:w-[40%]">
           {children}
         </div>
       </div>
