6e39fad1ad0afcac48b8fc917d46cbd39df615f3
M. Fadil Martias <64735243+fadilmartias@users.noreply.github.com>
2024-08-08 09:00:06 +0700

    setting model user

--- JavaScript/Node/express_auth_template/src/models/User.js
+++ JavaScript/Node/express_auth_template/src/models/User.js
@@ -14,12 +14,48 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.BIGINT.UNSIGNED
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    refresh_token: {
+      type: DataTypes.STRING
+    },
+    created_at: {
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      type: DataTypes.DATE,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      onUpdate: DataTypes.NOW
+    }
   }, {
     sequelize,
     modelName: 'User',
   });
   return User;
-};
+}
